835f0209fc615fe8e277ce9ee79d59ca453c2520
Thomas Guillory <thomasvguillory@gmail.com>
2024-04-22 09:14:24 +0200

    distribute script

--- distribute.sh
+++ distribute.sh
@@ -5,42 +5,45 @@ echo "====              Code signing and notarisation script                    
 echo "=============================================================================================================="
 echo "====                                                                                                      ===="
 echo "====     Usage:                                                                                           ===="
-echo "====     ./createPkgs.sh PluginName DeveloperId AppleID AppPass Identifier Version                        ===="
+echo "====     ./distribute.sh PluginName DeveloperId Identifier Version                                        ===="
+echo "====     Exemple:                                                                                         ===="
+echo "====     ./distribute.sh Synthy "Jon Doe (53B7GD2XBM)" com.Doe.name 1.0.1                                 ===="
 echo "====                                                                                                      ===="
 echo "====     PluginName = Plugin name without any extension - will try to create pkgs for VST/VST3/AU         ===="
 echo "====     Developer ID = \"Your Name (NNNNNNNNNN)\"                                                        ===="
-echo "====     Apple ID = emailaddress@somewhere.com                                                            ===="
-echo "====     AppPass = app-sepecific password generate at  https://appleid.apple.com/                         ===="
 echo "====     Identifier  = com.MyCompany.name                                                                 ===="
 echo "====     Version  = version number, i.e, 1.0.1                                                            ===="
 echo "=============================================================================================================="
 echo ""
-if [ "$EUID" -ne 0 ]
-  then echo "Error: Please run this command as root using sudo..."
-  echo ""
-  exit
-fi	
 
-# if [[ $# -lt 4 ]]; then
-#     echo "Error: Illegal number of parameters..."
-#     echo ""
-#     exit
-# fi
+
+if [[ $# -lt 4 ]]; then
+    echo "Error: Illegal number of parameters..."
+    echo ""
+    exit
+fi
 
 PLUGIN_NAME=$1
 DEVELOPER_ID=$2
-# APP_PASS=$3
-# APPLE_ID=$4
-IDENTIFIER=$5
-VERSION=$6
+IDENTIFIER=$3
+VERSION=$4
 
-#use pkgbuild to build the  plugin installers - and sign with Apple Developer ID 
-pkgbuild --install-location /Library/Audio/Plug-Ins/Components --identifier "$IDENTIFIER"au --version 1.0.2 --component "$PLUGIN_NAME.component" "$PLUGIN_NAME"_au.pkg 
-sudo codesign -s "Developer ID Application: $DEVELOPER_ID" "$PLUGIN_NAME".component --timestamp --force --deep
-pkgbuild --install-location /Library/Audio/Plug-Ins/VST3 --identifier "$IDENTIFIER"vst3 --version 1.0.2 --component "$PLUGIN_NAME".vst3 "$PLUGIN_NAME"vst3.pkg 
-sudo codesign -s "Developer ID Application: $DEVELOPER_ID" "$PLUGIN_NAME".vst3 --timestamp --force --deep
+
+# sign the AU binary
+codesign -s "Developer ID Application: $DEVELOPER_ID" --timestamp --force -o runtime -i "$PLUGIN_NAME".component "$PLUGIN_NAME".component/Contents/MacOS/"$PLUGIN_NAME"  #--options=runtime
+# package the .content and sign it
+pkgbuild --install-location /Library/Audio/Plug-Ins/Components --sign "Developer ID Installer: $DEVELOPER_ID" --timestamp --identifier "$IDENTIFIER"au --version "$VERSION" --root "$PLUGIN_NAME".component "$PLUGIN_NAME"_au.pkg 
+# sign the VST binary
+codesign -s "Developer ID Application: $DEVELOPER_ID" --timestamp --force -o runtime -i "$PLUGIN_NAME".vst3 "$PLUGIN_NAME".vst3/Contents/MacOS/"$PLUGIN_NAME"  #--options=runtime
+# package the .vst3 and sign it
+pkgbuild --install-location /Library/Audio/Plug-Ins/VST3 --sign "Developer ID Installer: $DEVELOPER_ID" --timestamp --identifier "$IDENTIFIER"vst3 --version "$VERSION" --root "$PLUGIN_NAME".vst3 "$PLUGIN_NAME"_vst3.pkg 
+# prepare the installer
 productbuild --synthesize --package "$PLUGIN_NAME"_au.pkg --package "$PLUGIN_NAME"_vst3.pkg distribution.xml
-productbuild --distribution distribution.xml --resources Resources/ ""$PLUGIN_NAME".pkg"
-productsign --sign "Developer ID Installer: $DEVELOPER_ID" "$PLUGIN_NAME".pkg "$PLUGIN_NAME"_installer.pkg
-xcrun notarytool submit --keychain-profile "thomas" "$PLUGIN_ID".pkg
-xcrun stapler staple "$PLUGIN_ID".pkg
+# build the installer
+productbuild --distribution distribution.xml --resources Resources/ "$PLUGIN_NAME".pkg
+# Sign the installer
+productsign --sign "Developer ID Installer: $DEVELOPER_ID" "$PLUGIN_NAME".pkg "$PLUGIN_NAME"_installer.pkg --timestamp
+# notarize the installer for gatekeeper
+xcrun notarytool submit --keychain-profile "thomas" "$PLUGIN_NAME"_installer.pkg --wait
+# staple the ticket to the installer
+xcrun stapler staple "$PLUGIN_NAME"_installer.pkg
