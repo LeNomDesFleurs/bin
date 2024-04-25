02a7b2f379271ce7aa29e3c10c44068de71044c1
Thomas Guillory <thomasvguillory@gmail.com>
2024-04-25 15:08:30 +0200

    correct distribute mistake

--- distribute.sh
+++ distribute.sh
@@ -32,11 +32,11 @@ VERSION=$4
 # sign the AU binary
 codesign -s "Developer ID Application: $DEVELOPER_ID" --timestamp --force -o runtime -i "$PLUGIN_NAME".component "$PLUGIN_NAME".component/Contents/MacOS/"$PLUGIN_NAME"  #--options=runtime
 # package the .content and sign it
-pkgbuild --install-location /Library/Audio/Plug-Ins/Components --sign "Developer ID Installer: $DEVELOPER_ID" --timestamp --identifier "$IDENTIFIER"au --version "$VERSION" --root "$PLUGIN_NAME".component "$PLUGIN_NAME"_au.pkg 
+pkgbuild --install-location ~/Library/Audio/Plug-Ins/Components --sign "Developer ID Installer: $DEVELOPER_ID" --timestamp --identifier "$IDENTIFIER"au --version "$VERSION" --component "$PLUGIN_NAME".component "$PLUGIN_NAME"_au.pkg 
 # sign the VST binary
 codesign -s "Developer ID Application: $DEVELOPER_ID" --timestamp --force -o runtime -i "$PLUGIN_NAME".vst3 "$PLUGIN_NAME".vst3/Contents/MacOS/"$PLUGIN_NAME"  #--options=runtime
 # package the .vst3 and sign it
-pkgbuild --install-location /Library/Audio/Plug-Ins/VST3 --sign "Developer ID Installer: $DEVELOPER_ID" --timestamp --identifier "$IDENTIFIER"vst3 --version "$VERSION" --root "$PLUGIN_NAME".vst3 "$PLUGIN_NAME"_vst3.pkg 
+pkgbuild --install-location ~/Library/Audio/Plug-Ins/VST3 --sign "Developer ID Installer: $DEVELOPER_ID" --timestamp --identifier "$IDENTIFIER"vst3 --version "$VERSION" --component "$PLUGIN_NAME".vst3 "$PLUGIN_NAME"_vst3.pkg 
 # prepare the installer
 productbuild --synthesize --package "$PLUGIN_NAME"_au.pkg --package "$PLUGIN_NAME"_vst3.pkg distribution.xml
 # build the installer
@@ -47,3 +47,8 @@ productsign --sign "Developer ID Installer: $DEVELOPER_ID" "$PLUGIN_NAME".pkg "$
 xcrun notarytool submit --keychain-profile "thomas" "$PLUGIN_NAME"_installer.pkg --wait
 # staple the ticket to the installer
 xcrun stapler staple "$PLUGIN_NAME"_installer.pkg
+# clean
+rm distribution.xml
+rm "$PLUGIN_NAME".pkg
+rm "$PLUGIN_NAME"_au.pkg
+rm "$PLUGIN_NAME"_vst3.pkg
