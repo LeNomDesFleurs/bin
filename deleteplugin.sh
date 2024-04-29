db4940b02726ed9fe901eec4a941320d4f331225
Thomas Guillory <thomasvguillory@gmail.com>
2024-04-29 08:04:15 +0200

    added plugin delete

--- deleteplugin.sh
+++ deleteplugin.sh
@@ -2,7 +2,11 @@
 
 PLUGIN_NAME=$1
 
-rm /Library/Audio/Plug-Ins/Components/"$PLUGIN_NAME".component
-rm ~/Library/Audio/Plug-Ins/Components/"$PLUGIN_NAME".component
-rm /Library/Audio/Plug-Ins/Components/"$PLUGIN_NAME".vst3
-rm ~/Library/Audio/Plug-Ins/Components/"$PLUGIN_NAME".vst3
+rm -rfv /Library/Audio/Plug-Ins/Components/"$PLUGIN_NAME".component
+rm -rfv ~/Library/Audio/Plug-Ins/Components/"$PLUGIN_NAME".component
+rm -rfv /Library/Audio/Plug-Ins/VST3/"$PLUGIN_NAME".vst3
+rm -rfv ~/Library/Audio/Plug-Ins/VST3/"$PLUGIN_NAME".vst3
+
+
+echo "try to delete a plugin in user and system directories, for au and vst3"
+echo "provide the name of the the plugin in argument, whitout extension"
